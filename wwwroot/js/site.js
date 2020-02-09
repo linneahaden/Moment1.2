1b093f9c4e3edd485c61d452fe98b9c28877141e
Linnéa Hådén <liha0500@student.miun.se>
2020-02-09 14:44:38 +0100

    Add project files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿"use strict";
+
+document.getElementById("logo").addEventListener("mouseover", mouseOver);
+document.getElementById("logo").addEventListener("mouseout", mouseOut);
+
+function mouseOver() {
+    document.getElementById("logo").style.fontFamily = "Special Elite";
+}
+
+function mouseOut() {
+    document.getElementById("logo").style.fontFamily = "Montserrat";
+}
